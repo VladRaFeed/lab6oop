d6073cadee89d094b91e84a29e13e0621da28a7b
Vinga <jack22toplolkek@gmail.com>
2024-06-08 10:05:59 +0300

    Добавьте файлы проекта.

---
+++ lab6oop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab6oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Its 6 lab of oop!");
+        }
+    }
+}
